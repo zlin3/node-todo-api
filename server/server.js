e224a39775c0047c14a193f76ef91036b1d0e575
zlin3 <tslzf@hotmail.com>
2018-03-24 23:38:40 -0700

    add DELTE /todos/id route

--- server/server.js
+++ server/server.js
@@ -47,6 +47,21 @@ app.get('/todos/:id', (req, res) => {
   //res.send(req.params);
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`starting on port ${port}`);
 });
