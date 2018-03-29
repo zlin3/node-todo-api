c02c60dc05ddff5c1046214f4006b97775846da5
zlin3 <tslzf@hotmail.com>
2018-03-29 11:46:28 -0700

    add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -96,7 +97,6 @@ app.post('/users', (req, res) => {
 
   user.save().then(() => {
     return user.generateAuthToken();
-    //res.send(doc);
   }).then((token) => {
     res.header('x-auth', token).send(user);
   }).catch((err) => {
@@ -106,6 +106,17 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  User.findByCrendential(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(port, () => {
