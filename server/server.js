ecf4b4dab9e87bde6bfccc0596e1bfd37fc7b0c1
zlin3 <tslzf@hotmail.com>
2018-03-28 21:45:20 -0700

    add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -89,6 +89,20 @@ app.patch('/todos/:id', (req, res) => {
     })
 });
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+    //res.send(doc);
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
+});
+
 app.listen(port, () => {
   console.log(`starting on port ${port}`);
 });
